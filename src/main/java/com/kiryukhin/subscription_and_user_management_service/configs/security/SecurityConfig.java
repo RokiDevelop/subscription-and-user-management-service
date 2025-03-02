d1ee350c502cb8c691e53897589e3e9053530975
Roman_Kiryukhin <rkiryukhin@gmail.com>
2025-03-02 16:58:00 +0700

    api start setup

---
+++ src/main/java/com/kiryukhin/subscription_and_user_management_service/configs/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.kiryukhin.subscription_and_user_management_service.configs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("*").permitAll());
+
+        SecurityFilterChain chain = httpSecurity.build();
+        return chain;
+    }
+}
