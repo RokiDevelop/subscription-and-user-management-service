5e04a9437edeba3241c468fa99d03933890c6001
Roman_Kiryukhin <rkiryukhin@gmail.com>
2025-03-02 12:41:16 +0700

    Project initialization

---
+++ src/main/java/com/kiryukhin/subscription_and_user_management_service/SubscriptionAndUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kiryukhin.subscription_and_user_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionAndUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionAndUserManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiryukhin/subscription_and_user_management_service/SubscriptionAndUserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiryukhin.subscription_and_user_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionAndUserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
